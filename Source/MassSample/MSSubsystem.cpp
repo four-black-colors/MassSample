674f75a884cf3d2f3dc37c770191988346f53a63
Peter Matejovsky <cupsster@gmail.com>
2022-04-29 20:31:27 +0200

    Reformatted code and converted raw pointer to TObjectPtr as suggested by Epic

--- Source/MassSample/MSSubsystem.cpp
+++ Source/MassSample/MSSubsystem.cpp
@@ -14,54 +14,40 @@ void UMSSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
 	const UWorld* World = GetWorld();
 
-	//cache our Mass Entity Subsystem
+	// Cache our Mass Entity Subsystem
 	EntitySystem = World->GetSubsystem<UMassEntitySubsystem>();
 
-
-	//To spawn entities from C++ we can make a new archetype like so:
-	MoverArchetype =  EntitySystem->CreateArchetype(
+	// To spawn entities from C++ we can make a new archetype like so:
+	MoverArchetype = EntitySystem->CreateArchetype(
 	{
 		FTransformFragment::StaticStruct(),
 		FMassVelocityFragment::StaticStruct()
 	});
 
-	Context =  EntitySystem->CreateExecutionContext(0);
-	
-	
+	Context = EntitySystem->CreateExecutionContext(0);
 }
 
 int32 UMSSubsystem::SpawnEntity()
 {
+	// Create and store a handle to our new entity
+	const FMassEntityHandle NewEntity = EntitySystem->CreateEntity(MoverArchetype);
 
+	// Afterwards you can add, remove or change fragments of the entity. Let's add a tag fragment!
+	EntitySystem->AddTagToEntity(NewEntity, FMoverTag::StaticStruct());
+	// Make sure you use AddTag for tags and AddFragment for fragments!
+	EntitySystem->AddFragmentToEntity(NewEntity, FSampleColorFragment::StaticStruct());
 
-	//Create and store a handle to our new entity
-	FMassEntityHandle NewEntity = EntitySystem->CreateEntity(MoverArchetype);
-
-
-	
-	//Afterwards you can add, remove or change fragments of the entity. Let's add a tag fragment!
-	EntitySystem->AddTagToEntity(NewEntity,
-		FMoverTag::StaticStruct());
-	//Make sure you use AddTag for tags and AddFragment for fragments!
-	EntitySystem->AddFragmentToEntity(NewEntity,
-	FSampleColorFragment::StaticStruct());
-
-	
-	//How about changing data on a fragment?
+	// How about changing data on a fragment?
 	EntitySystem->GetFragmentDataChecked<FMassVelocityFragment>(NewEntity).Value = FMath::VRand()*100.0f;
 	EntitySystem->GetFragmentDataChecked<FSampleColorFragment>(NewEntity).Color = FColor::Blue;
 
-	
+	// Calling mass.PrintEntityFragments 1 in editor should reveal this entity's fragments! For example:
 
-	//calling mass.PrintEntityFragments 1 in editor should reveal this entity's fragments! For example:
-
-	// 	Listing fragments values for Entity[i: 1 sn: 1] in /Game/ThirdPerson/Maps/ThirdPersonMap.ThirdPersonMap:MassEntitySubsystem_0
+	// Listing fragments values for Entity[i: 1 sn: 1] in /Game/ThirdPerson/Maps/ThirdPersonMap.ThirdPersonMap:MassEntitySubsystem_0
 	// TransformFragment: (Transform=(Rotation=(X=0.000000,Y=0.000000,Z=0.000000,W=1.000000),Translation=(X=0.000000,Y=0.000000,Z=0.000000),Scale3D=(X=1.000000,Y=1.000000,Z=1.000000)))
 	// MassVelocityFragment: ()
 	// SampleColorFragment: ()
-	UE_LOG( LogTemp, Warning, TEXT("%i entity created on frame %i"),	NewEntity.Index,GFrameCounter);
+	UE_LOG(LogTemp, Warning, TEXT("%i entity created on frame %i"),	NewEntity.Index, GFrameCounter);
 
 	return NewEntity.Index;
-	
 }
-
--- Source/MassSample/MSSubsystem.h
+++ Source/MassSample/MSSubsystem.h
@@ -3,12 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "UObject/ObjectPtr.h"
 #include "MassEntitySubsystem.h"
 #include "Common/Fragments/MSHashGridFragments.h"
 #include "MSSubsystem.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class MASSSAMPLE_API UMSSubsystem : public UWorldSubsystem
@@ -16,13 +17,13 @@ class MASSSAMPLE_API UMSSubsystem : public UWorldSubsystem
 	GENERATED_BODY()
 public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
-	
+
 	UPROPERTY(Transient)
-	UMassEntitySubsystem* EntitySystem;
+	TObjectPtr<UMassEntitySubsystem> EntitySystem;
 	FMassArchetypeHandle MoverArchetype;
 
 	FMSHashGrid3D HashGrid = FMSHashGrid3D(100.0f,FMassEntityHandle());
-	
+
 	FMassExecutionContext Context;
 
 	UFUNCTION(BlueprintCallable)
